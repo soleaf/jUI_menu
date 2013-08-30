536e732b3312a406332d5fed0eff803dd9d4f973
soleaf <soleaf@gmail.com>
2013-08-30 11:25:46 +0900

    English note and Example

--- js/org.mintcode.jUI_menu.js
+++ js/org.mintcode.jUI_menu.js
@@ -9,37 +9,38 @@ GitHub : https://github.com/soleaf/jUI_menu
 
 $(document).ready(function(){
 
-	// 바디 클릭시 레이러 사라지게
+	// Click to body: Hide all menus.
 	$(document).bind("click",function() {
 		jUI_menu_layerHideAll();
 	});
 	
-	// 메뉴 레이어를 바디로 옮기기:포지션 문제
+	// Move Menu Layers definend anywehere to Body root.
 	$("body").prepend($(".jUI_memu_layer"));
 	$(".pageContent .jUI_memu_layer").remove();
 	
-	// 메뉴객체 찾아내기
+	// Search menu objects for event
 	$(".jUI_menu").each(function(){
 		
 		var senderSelector = ".jUI_menu[data-id=" + $(this).attr("data-id") + "]";
 		var menuSelector = ".jUI_memu_layer[data-id=" + $(this).attr("data-id") + "]";
 		var allMenuSelector = ".jUI_memu_layer[data-root=" + $(this).attr("data-id") + "]";
 		
-		// 선택된 값이 지정되어있나?
+		// Select item that is setted selectedValue
+		// Checking Selected value
 		var selectedValue = "";
 		var selectedLabel = "";
 		if(null != $(this).attr("data-selectedValue") && $(this).attr("data-selectedValue").length > 0) {
 			selectedValue = $(this).attr("data-selectedValue")
 			selectedLabel = $(allMenuSelector).find("LI[data-value="+ selectedValue +"]").text();
 		}
-		else // 선택X -> 첫번째 아이템을 자동선택
+		else // Null is setted selected value -> force select first item.
 		{
 			selectedValue = $(menuSelector).children("LI:first-child").attr("data-value");
 			selectedLabel = $(menuSelector).children("LI:first-child").text();
 		}
 		$(this).find(".label").text(selectedLabel);
 		
-		// 메뉴에서 서브메뉴가있으면 화살표 주기
+		// Submenu have arrow mark.
 		$(menuSelector).find("LI").each(function(){
 			var isHaveSub = $(this).attr("data-sub");
 			if (isHaveSub != null){
@@ -50,17 +51,18 @@ $(document).ready(function(){
 			}
 		});
 		
-		// 너비 지정: data-width이 있으면 강제 지정
+		// Width: if have element of data-width, set force it.
 		var data_width = $(this).attr("data-width");
 		var menuSenderW = $(this).width();
-		var menuW = $(menuSelector).width(); // 26: arrow width 포함
+		var menuW = $(menuSelector).width(); // 26: arrow width
 		
 		if (data_width > 0){
 			$(menuSelector).css("width", data_width);
 			$(this).css("width", data_width);
 		}
+		// Auto with
 		else{
-			// 속성이 없으면 자동너비 맞춤: 큰쪽기준
+			
 			if (menuSenderW > menuW){
 				$(menuSelector).css("width", menuSenderW);
 			}	
@@ -70,7 +72,7 @@ $(document).ready(function(){
 				
 		}
 		
-		// 1. .jUI_menu click 이벤트 연결
+		// 1. Bind Event to .UI_menu click
 		//$(this).bind("mouseover",function(){
 		$(this).bind("click",function(){
 			
@@ -81,7 +83,7 @@ $(document).ready(function(){
 			return false;
 		});
 		
-		// 2. 메뉴 선택후 값을 전달할 객체 생성
+		// 2. INPUT FOR FORM
 		var formInput = "<input type='text' name='" + $(this).attr("data-id") + "' value='" + selectedValue + "' style='display:none' data-role='jUI_menu'/>";
 		$(this).append(formInput);
 		
@@ -92,15 +94,14 @@ $(document).ready(function(){
 	
 });
 
-/* 클릭 메서드 */
+/* CLICK EVENT */
 function jUI_menu_click(senderSelector, data_root){
 
-//	console.log("이 메뉴를 소유한 버튼 id: " + data_root);
 	var isCallSubMenu = false;
 	
-	// 2. this.data-sub인지 확인 -> data-sub인경우 메인메뉴에서 서브메뉴를 호출하는 과정임
+	// 2. this.data-sub -> call submenu form main menu
 	if( $(senderSelector).attr("data-sub") == null){
-		// 모든 메뉴 삭제
+		// hide all menu
 		jUI_menu_layerHideAll();
 		isCallSubMenu = false;
 	}
@@ -109,7 +110,7 @@ function jUI_menu_click(senderSelector, data_root){
 		isCallSubMenu = true;
 	}
 	
-	// 3. this.data-injection과 일치하는 .jUI_menu_layer를 선택
+	// 3. Select .jUI_menu_layer with maching this.data-injection
 	var data_injection = "";
 	if(isCallSubMenu){
 		data_injection = $(senderSelector).attr("data-sub");
@@ -119,13 +120,13 @@ function jUI_menu_click(senderSelector, data_root){
 	}
 	var menuSelector = ".jUI_memu_layer[data-id=" + data_injection + "]";
 	
-	// 4. 서브메뉴 확인 후 이벤트 연결
+	// 4. Bind event after checking submenu
 	$(menuSelector).children("LI").each(function(i) {
 		var data_sub = $(this).attr("data-sub");
 		var data_value = $(this).attr("data-value");
 		
 		if(data_sub != null){
-			// 서브메뉴를 연결
+			// bind
 			//$(this).bind("mouseover",function(){
 			$(this).unbind("click");
 			$(this).bind("click",function(){
@@ -135,7 +136,7 @@ function jUI_menu_click(senderSelector, data_root){
 			});
 		}
 		else{
-			// 루트 메뉴버튼 값에 반영
+			// change input value
 			$(this).one("click",function(){
 	
 				var roleOfDiv = $(this).parent().attr("data-role");
@@ -167,7 +168,7 @@ function jUI_menu_click(senderSelector, data_root){
 		}
 	});
 	
-	// 5. 좌표설정
+	// 5. POS
 	var marginX = 0;
 	var marginY = 2;
 	if (isCallSubMenu){
@@ -178,10 +179,10 @@ function jUI_menu_click(senderSelector, data_root){
 	$(menuSelector).css("left",menuLocation["x"]);
 	$(menuSelector).css("top",menuLocation["y"]);
 
-	// 가로너비 지정
+	// width
 	//$(menuSelector).css("min-width", $(senderSelector).width());
 	
-	// 6. 애니메이션
+	// 6. Animation
 	$(menuSelector).clearQueue().slideDown(250);
 	
 	return false;
@@ -189,7 +190,7 @@ function jUI_menu_click(senderSelector, data_root){
 }
 
 
-/* 레이어 좌표를 계산하기 */
+/* CALCULATE MENU POS */
 function jUI_menu_calPosition(obj, marginLeft, marginTop, isCallSubMenu)
 {
 	var location = new Array();
@@ -206,7 +207,7 @@ function jUI_menu_calPosition(obj, marginLeft, marginTop, isCallSubMenu)
 	return location;
 }
 
-// 메뉴 숨기기
+// HIDE ALL MENU
 function jUI_menu_layerHideAll()
 {
 	$(".jUI_memu_layer").each(function() {
